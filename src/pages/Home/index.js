925a1a91e242da87de2476d63fc09c9b27b350e0
Rodrigo Cândido da Silva <rodrigo_candido.dev@outlook.com>
2020-03-27 18:38:16 -0300

    home page structure added

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import  {FiPlus} from 'react-icons/fi';
+
 import './styles.css';
 
 export default function Home(){
     return(
-        <h1></h1>
+        <div className="all-lists-container">
+            <section className="list">
+                <div className="title-box">
+                    <h1>To-Do Lists</h1>
+                </div>
+                <div className="actions-box">
+                    <button className="button" type="button">
+                        <FiPlus size={16} color="#ccc" />
+                    </button>
+                </div>
+                <div className="lists-panel">
+                    <ul>
+                        <li>
+                            <strong>Lista de Segunda</strong>
+                            <div className="list-creater-box">
+                                <span>R</span>
+                                <p>Rodrigo</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </div>
     );
 }
